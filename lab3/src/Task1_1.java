e2d8f305f1159d9c2009590233d053b2350413a5
WellDone <buddy11081@gmail.com>
2024-03-12 23:11:44 +0200

    lab3

---
+++ lab3/src/Task1_1.java
@@ -0,0 +1,9 @@
+public class Task1_1 {
+
+    public Task1_1 () {
+        for (int a = 0; a < 50; a++) {
+            System.out.println("Ya liubliu Programuvannya z ciklom for");
+
+        }
+    }
+}
---
+++ lab3/src/Task1_2.java
@@ -0,0 +1,10 @@
+public class Task1_2 {
+
+    public Task1_2() {
+        int i = 0;
+        while (i < 50) {
+            System.out.println("Ya liubliu Programuvannya z ciklom while");
+            i++;
+        }
+    }
+}

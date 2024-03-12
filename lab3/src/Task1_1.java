f13a0c7b6cf2c8a612452fa756197d5e98ef690a
Nikita <127943860+NickBondarenko17@users.noreply.github.com>
2024-03-12 23:15:56 +0200

    Delete lab3 directory

--- lab3/src/Task1_1.java
+++
@@ -1,9 +0,0 @@
-public class Task1_1 {
-
-    public Task1_1 () {
-        for (int a = 0; a < 50; a++) {
-            System.out.println("Ya liubliu Programuvannya z ciklom for");
-
-        }
-    }
-}

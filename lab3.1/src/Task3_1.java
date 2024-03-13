4bf658204740ddabe0147d8bb7761d12434c825b
WellDone <buddy11081@gmail.com>
2024-03-13 16:06:32 +0200

    lab3

--- lab3.1/src/Task3_1.java
+++ lab3.1/src/Task3_1.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
 
 public class Task3_1 {
 
 
-    public Task3_1 () {
-            for (int i = 0; i < 50; i++) {
-                System.out.println("Ya liubliu Programuvannya");
-            }
+    public Task3_1() {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter your Word:");
+        String word = Scanner.next();
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println(word);
         }
     }
-
-
+}

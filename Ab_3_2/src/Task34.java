3d7993ef8843450430ffff11dd9554503a7517dd
Nikita <127943860+NickBondarenko17@users.noreply.github.com>
2024-03-20 11:54:52 +0200

    Delete Ab_3_2/src directory

--- Ab_3_2/src/Task34.java
+++
@@ -1,9 +0,0 @@
-public class Task34 {
-    void Timerfor() {
-        for (int h = 0; h <= 2; h++) {
-            for (int min = 0; min < 60; min++) {
-                System.out.println(h + " h " + min + " min");
-            }
-        }
-    }
-}
